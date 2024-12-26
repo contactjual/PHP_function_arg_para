45f2cf911a209782cc4edc913e18970d270d00aa
Jual Rana <contactjual@gmail.com>
2024-12-27 00:22:00 +0600

    funciton

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+function new_function ($first, $second, $third) {
+    $sum = $first + $second + $third;
+    echo $sum;
+}
+new_function(2, 4, 5);
+
+function new_function ( int $first, int $second, int $third) {
+    $sum = $first + $second + $third;
+    echo $sum;gti 
+}
+// new_function(2, 4, "5"); // error: couse third argument must be a int type data
+
+
+?>
